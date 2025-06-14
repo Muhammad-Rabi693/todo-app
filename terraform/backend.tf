eac437745b4515d9152f42ee2a9f46cb91fc47ed
Muhammad-Rabi693 <rktiger693@gmail.com>
2025-06-14 20:00:34 +0500

    Second Commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "anas-terraform-state-bucket"
+    bucket       = "rabi-terraform-state-bucket-42"
     key          = "todo-swarm/terraform.tfstate"
     region       = "us-east-1"    
     encrypt      = true
